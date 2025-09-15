3728397baa6d3acc9eb2dda457882591dcef0c70
yastremskiyartemiy <ayastremskiy25@uwcchina.org>
2025-09-15 13:31:10 +0800

    ch

---
+++ logicalOperationsPython.py
@@ -0,0 +1,12 @@
+temp = 20
+sunny = True
+
+if temp <= 0 or temp >= 30:
+    print("The temperature is bad")
+else:
+    print("The temperature is good")
+
+if not sunny:
+    print("It is cloudy")
+else:
+    print("It is sunny")
